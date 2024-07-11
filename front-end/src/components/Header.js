dd14e2efa8b54beec1dd2ff90df87f051f85011b
ShadiMasarwa <masarwa@gmail.com>
2024-07-11 15:03:32 +0300

    Enter task when enter pressed + format header

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -2,6 +2,14 @@ import React from "react";
 import addIcon from "../icons/AddButton.svg";
 
 const Header = ({ newTaskText, setNewTaskText, addTask }) => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      if (newTaskText !== "") {
+        addTask();
+      }
+    }
+  };
+
   return (
     <>
       <div className="d-flex justify-content-center mb-5">
@@ -9,8 +17,9 @@ const Header = ({ newTaskText, setNewTaskText, addTask }) => {
           type="text"
           value={newTaskText}
           onChange={(e) => setNewTaskText(e.target.value)}
+          onKeyDown={(e) => handleKeyDown(e)}
           placeholder="Enter Task..."
-          className="inputForm form-control"
+          className="inputForm-header form-control"
         />
         <img
           src={addIcon}
